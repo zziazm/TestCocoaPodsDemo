f5720ef31e43571784beaeafa44bbdc925c76d4f
zziazm <1310726454@qq.com>
2017-07-12 16:08:28 +0800

    Initial Commit

---
+++ TestCocoaPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestCocoaPods
+//
+//  Created by 赵铭 on 2017/7/12.
+//  Copyright © 2017年 zm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestCocoaPods/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCocoaPods
+//
+//  Created by 赵铭 on 2017/7/12.
+//  Copyright © 2017年 zm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
