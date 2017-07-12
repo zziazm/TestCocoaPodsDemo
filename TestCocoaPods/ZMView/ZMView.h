00101cfc350d4cb6940d3d6c1e79a3f11004798f
zziazm <1310726454@qq.com>
2017-07-12 16:33:07 +0800

    update

---
+++ TestCocoaPods/ZMView/ZMView.h
@@ -0,0 +1,13 @@
+//
+//  ZMView.h
+//  TestCocoaPods
+//
+//  Created by 赵铭 on 2017/7/12.
+//  Copyright © 2017年 zm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZMView : UIView
+- (void)testCocoaPods;
+@end
---
+++ TestCocoaPods/ZMView/ZMView.m
@@ -0,0 +1,22 @@
+//
+//  ZMView.m
+//  TestCocoaPods
+//
+//  Created by 赵铭 on 2017/7/12.
+//  Copyright © 2017年 zm. All rights reserved.
+//
+
+#import "ZMView.h"
+
+@implementation ZMView
+- (void)testCocoaPods{
+    
+}/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
